44b473dadb77dc1d66c5a8f9520e99efac4fb08b
Inae Choi <inaechoi89@gmail.com>
2022-12-15 18:05:24 +0900

    feat: 다리 길이 입력받기

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -1,8 +1,10 @@
+const InputView = require('../view/InputView');
 const OutputView = require('../view/OutputView');
 
 class GameController {
   start() {
     OutputView.printStart();
+    const size = InputView.readBridgeSize();
   }
 }
 
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,3 +1,7 @@
+const { Console } = require('@woowacourse/mission-utils');
+const Message = require('../constant/InputMessage');
+const BridgeSize = require('../model/BridgeSize');
+const UserError = require('../util/UserError');
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -5,7 +9,15 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    let size = 0;
+    Console.readLine(Message.INPUT_SIZE, answer => {
+      this.validateSize(answer);
+      size = answer;
+    });
+    Console.print(size);
+    return size;
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
@@ -16,6 +28,23 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
+
+  validateSize(size) {
+    try {
+      const inputSize = new BridgeSize(size);
+    } catch (error) {
+      this.checkUserError(error);
+      Console.print(error);
+      this.readBridgeSize();
+    }
+  },
+
+  checkUserError(error) {
+    if (error instanceof UserError) {
+      return;
+    }
+    throw error;
+  },
 };
 
 module.exports = InputView;
