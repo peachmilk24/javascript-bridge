2084ef897642ed2a40eca81dbb6210409e9f7cda
Inae Choi <inaechoi89@gmail.com>
2022-12-16 17:27:12 +0900

    feat: 출력 메시지 추가

--- src/constant/PrintMessage.js
+++ src/constant/PrintMessage.js
@@ -1,6 +1,11 @@
 const Message = {
   START: '다리 건너기 게임을 시작합니다.',
   INPUT_SIZE: '다리의 길이를 입력해주세요.',
+  INPUT_DIRECTION: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
+  INPUT_RETRY: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+  FINAL_MAP: '최종 게임 결과',
+  RESULT: '게임 성공 여부: ',
+  TRY_COUNT: '총 시도한 횟수: ',
 };
 
 module.exports = Message;
