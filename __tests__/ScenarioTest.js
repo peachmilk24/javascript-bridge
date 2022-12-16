90a1c52a14e0907ce6a775a9f6ec8e6dd05d419a
Inae Choi <inaechoi89@gmail.com>
2022-12-16 23:48:54 +0900

    test: 시나리오 테스트 추가

---
+++ __tests__/ScenarioTest.js
@@ -0,0 +1,122 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const App = require('../src/App');
+const BridgeMaker = require('../src/BridgeMaker');
+
+const mockQuestions = answers => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((_, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = numbers => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+const getOutput = logSpy => {
+  return [...logSpy.mock.calls].join('');
+};
+
+const runException = inputs => {
+  mockQuestions(inputs);
+  const logSpy = getLogSpy();
+  const app = new App();
+
+  app.play();
+
+  expectLogContains(getOutput(logSpy), ['[ERROR]']);
+};
+
+const expectLogContains = (received, logs) => {
+  logs.forEach(log => {
+    expect(received).toEqual(expect.stringContaining(log));
+  });
+};
+
+const expectBridgeOrder = (received, upside, downside) => {
+  const upsideIndex = received.indexOf(upside);
+  const downsideIndex = received.indexOf(downside);
+
+  expect(upsideIndex).toBeLessThan(downsideIndex);
+};
+
+describe('다리 건너기 테스트', () => {
+  test('다리 생성 테스트2', () => {
+    const randomNumbers = [1, 0, 0, 1, 1, 1, 0, 0];
+    const mockGenerator = randomNumbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, jest.fn());
+
+    const bridge = BridgeMaker.makeBridge(8, mockGenerator);
+    expect(bridge).toEqual(['U', 'D', 'D', 'U', 'U', 'U', 'D', 'D']);
+  });
+
+  test('재시도 후 성공 시나리오', () => {
+    const logSpy = getLogSpy();
+    mockRandoms([1, 0, 0]);
+    mockQuestions(['3', 'U', 'U', 'R', 'U', 'D', 'D']);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      '[ O ]',
+      '[   ]',
+      '[ O | X ]',
+      '[   |   ]',
+      'R',
+      '[ O ]',
+      '[   ]',
+      '[ O |   ]',
+      '[   | O ]',
+      '[ O |   |   ]',
+      '[   | O | O ]',
+      '최종 게임 결과',
+      '[ O |   |   ]',
+      '[   | O | O ]',
+      '게임 성공 여부: 성공',
+      '총 시도한 횟수: 2',
+    ]);
+    expectBridgeOrder(log, '[ O |   |   ]', '[   | O | O ]');
+  });
+
+  test('실패 시나리오', () => {
+    const logSpy = getLogSpy();
+    mockRandoms([1, 0, 0]);
+    mockQuestions(['3', 'U', 'U', 'Q']);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      '[ O ]',
+      '[   ]',
+      '[ O | X ]',
+      '[   |   ]',
+      'Q',
+      '최종 게임 결과',
+      '[ O | X ]',
+      '[   |   ]',
+      '게임 성공 여부: 실패',
+      '총 시도한 횟수: 1',
+    ]);
+    expectBridgeOrder(log, '[ O | X ]', '[   |   ]');
+  });
+
+  test('예외 테스트', () => {
+    runException(['a']);
+  });
+});
