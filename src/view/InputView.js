e10c221ea6cfa7938a0b1c7e15a555c62a061f1b
Inae Choi <inaechoi89@gmail.com>
2022-12-16 21:10:03 +0900

    feat: 재시작 여부 입력받기

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -36,7 +36,15 @@ const InputView = {
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    let command = '';
+    Console.readLine(Message.INPUT_RETRY, answer => {
+      this.validateCommand(answer);
+      Console.print(answer);
+      command = answer;
+    });
+    return command;
+  },
 
   validateSize(size) {
     try {
